059d1865f1d9ec25bc208acf000896853b269932
Stas <staszinovatnyj@gmail.com>
2023-02-02 22:06:32 +0200

    pohyi

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Dish from "../dish/dish";
+import Header from "../header";
 
 import "./app.css";
 
 const App = () => {
   return (
     <div className="app">
+      <Header />
       <div className="row">
         <div className="col-1 col-sm-1 col-md-2 col-lg-3"></div>
         <div className="col-10 col-sm-10 col-md-8 col-lg-6">
