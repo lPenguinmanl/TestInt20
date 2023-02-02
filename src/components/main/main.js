b81aa903bd5e82b8ec126f617461c5276e525730
Stas <staszinovatnyj@gmail.com>
2023-02-03 00:32:38 +0200

    add and upgrade header

---
+++ src/components/main/main.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import "./main.css";
+
+const Main = () => {
+  return (
+    <div>
+      <div className="main-welc">
+        <h1>welcome</h1>
+        <p>
+          Discover delicious inspiration and essential ingredients for every
+          meal with our comprehensive recipe and product hub
+        </p>
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Main;
