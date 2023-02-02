55908fff714692925b806e8aa0854936f690a0fc
Stas <staszinovatnyj@gmail.com>
2023-02-02 23:18:53 +0200

    pizda

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,21 +5,21 @@ import "./header.css";
 const Header = () => {
   return (
     <div className="row header">
-      <div className="col-2"></div>
-      <div className="col-1">
+      <div className="col-sm-0 col-md-1 col-lg-2"></div>
+      <div className="col-sm-2 col-md-2 col-lg-1">
         <p>Main</p>
       </div>
-      <div className="col-1">
+      <div className="col-sm-2 col-md-2 col-lg-1">
         <p>Dishes</p>
       </div>
-      <div className="col-1">
+      <div className="col-sm-2 col-md-2 col-lg-1">
         <p>My products</p>
       </div>
-      <div className="col-1"></div>
-      <div className="col-4">
+      <div className="col-sm-0 col-md-1 col-lg-1"></div>
+      <div className="col-sm-5 col-md-3 col-lg-4">
         <input type="search"></input>
       </div>
-      <div className="col-2"></div>
+      <div className="col-sm-0 col-md-1 col-lg-2"></div>
     </div>
   );
 };
