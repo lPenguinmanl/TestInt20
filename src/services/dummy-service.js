ecef3ddb60a65ca663ba651feff7f97c7fe56a25
camelCase0 <pashok2082.Ryuz@gmail.com>
2023-02-06 22:51:38 +0200

    HOC + Dummy + DishList data

--- src/services/dummy-service.js
+++ src/services/dummy-service.js
@@ -1,5 +1,5 @@
-class MealService {
-  getDishes() {
+export default class MealService {
+  getAllDishes = async () => {
     return [
       {
         meal_id: 1,
@@ -7,6 +7,72 @@ class MealService {
         meal_image: "string",
         difficulty: 2,
       },
+      {
+        meal_id: 2,
+        meal_name: "Kabaka",
+        meal_image: "string",
+        difficulty: 5,
+      },
+      {
+        meal_id: 3,
+        meal_name: "Fish",
+        meal_image: "string",
+        difficulty: 2,
+      },
+      {
+        meal_id: 4,
+        meal_name: "Polovnik",
+        meal_image: "string",
+        difficulty: 2,
+      },
+      {
+        meal_id: 5,
+        meal_name: "Marinad",
+        meal_image: "string",
+        difficulty: 2,
+      },
+      {
+        meal_id: 6,
+        meal_name: "Pidor",
+        meal_image: "string",
+        difficulty: 2,
+      },
+      {
+        meal_id: 7,
+        meal_name: "Kopcheniy",
+        meal_image: "string",
+        difficulty: 2,
+      },
+      {
+        meal_id: 8,
+        meal_name: "LOH",
+        meal_image: "string",
+        difficulty: 2,
+      },
+      {
+        meal_id: 9,
+        meal_name: "HOTDOG",
+        meal_image: "string",
+        difficulty: 2,
+      },
+      {
+        meal_id: 10,
+        meal_name: "Henesy",
+        meal_image: "string",
+        difficulty: 2,
+      },
+      {
+        meal_id: 11,
+        meal_name: "Bylka",
+        meal_image: "string",
+        difficulty: 2,
+      },
+      {
+        meal_id: 12,
+        meal_name: "Vodka",
+        meal_image: "string",
+        difficulty: 2,
+      },
     ];
-  }
+  };
 }
