2b6427e21ed950f130e42b00b1a097d770e1bb2f
camelCase0 <pashok2082.Ryuz@gmail.com>
2023-02-06 20:33:29 +0200

    Routing

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,7 +1,9 @@
 import React from "react";
 import Dish from "../dish/dish";
+import DishList from "../dish-list";
 import Header from "../header";
 import Main from "../main";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./app.css";
 
@@ -9,7 +11,11 @@ const App = () => {
   return (
     <div className="app">
       <Header />
-      <Main />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/dishes" element={<DishList />} />
+        <Route path="/fridge" element={null} />
+      </Routes>
       <footer>
         <p>hyeta</p>
       </footer>
--- src/components/main/main.js
+++ src/components/main/main.js
@@ -7,7 +7,7 @@ const Main = () => {
   return (
     <div className="main-div-main">
       <div className="main-img">
-        <img src="https://s3-alpha-sig.figma.com/img/1fa5/8dd3/ad2935d1760be0c780beb158ef8c465b?Expires=1676246400&Signature=DWV5KAlJn0Hv3wanb4E8lFipxyJLxGJU9j0p5yQuh4FagWLHWwtbpyoSyIa7kFGrZ-2fhYJSIekT7hj~8-RYQ8ePpcfuRT~eNowfaA60Xq~QsbaQkxf8ThFdMa5hYBNeFKBNXliWVoAcaDFa8Tk3VeawGMCT9yt7GgpQlSazgsEWD4EhBpFv44qFJhdkCL1G2mWty5VfbBQ6eZo0n3JFRoocpeWsu72ChQb2wDFrUmbNEI5rxOXDMZ3AjVT2X4aQlq7yqwFs8CBmvS7wS5wBah3ks4f0WP0KT29Al-mOBvyFHuF2v8rwq7960K-m87nhBWDt3Zsq67JmpeJTsnCSBw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"></img>
+        <img src="https://s3-alpha-sig.figma.com/img/1fa5/8dd3/ad2935d1760be0c780beb158ef8c465b?Expires=1676246400&Signature=DWV5KAlJn0Hv3wanb4E8lFipxyJLxGJU9j0p5yQuh4FagWLHWwtbpyoSyIa7kFGrZ-2fhYJSIekT7hj~8-RYQ8ePpcfuRT~eNowfaA60Xq~QsbaQkxf8ThFdMa5hYBNeFKBNXliWVoAcaDFa8Tk3VeawGMCT9yt7GgpQlSazgsEWD4EhBpFv44qFJhdkCL1G2mWty5VfbBQ6eZo0n3JFRoocpeWsu72ChQb2wDFrUmbNEI5rxOXDMZ3AjVT2X4aQlq7yqwFs8CBmvS7wS5wBah3ks4f0WP0KT29Al-mOBvyFHuF2v8rwq7960K-m87nhBWDt3Zsq67JmpeJTsnCSBw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4" alt="imck"></img>
         <div className="main-text">
           <h1>welcome</h1>
           <p>
