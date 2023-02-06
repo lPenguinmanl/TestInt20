bb5b7936ec475a2e4cf7231cd8e97891aaac789d
Stas <staszinovatnyj@gmail.com>
2023-02-06 20:56:26 +0200

    upgrade

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,10 +1,5 @@
 import React from "react";
-import Dish from "../dish/dish";
-<<<<<<< HEAD
 import Dishes from "../dishes/dishes";
-=======
-import DishList from "../dish-list";
->>>>>>> 66d684bcd0d17585a0dd4f216f15300bcfa98e99
 import Header from "../header";
 import Main from "../main";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -15,18 +10,11 @@ const App = () => {
   return (
     <div className="app">
       <Header />
-<<<<<<< HEAD
-      <Dishes />
-=======
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="/dishes" element={<DishList />} />
+        <Route path="/dishes" element={<Dishes />} />
         <Route path="/fridge" element={null} />
       </Routes>
-      <footer>
-        <p>hyeta</p>
-      </footer>
->>>>>>> 66d684bcd0d17585a0dd4f216f15300bcfa98e99
     </div>
   );
 };
