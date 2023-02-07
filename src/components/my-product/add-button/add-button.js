7614167c4119952e0d8b0d6f964d2ca158be9752
Stas <staszinovatnyj@gmail.com>
2023-02-07 17:20:56 +0200

    a y menia est

--- src/components/my-product/add-button/add-button.js
+++ src/components/my-product/add-button/add-button.js
@@ -7,30 +7,39 @@ const AddButton = () => {
     <div className="add-button">
       <h3 className="add-button-text">Add Product On Your Freezer</h3>
       <div className="add-button-group">
-        <h3 className="add-button-text-h">Product:</h3>
-        <input
-          className="add-button-input"
-          type="text"
-          id="name-product"
-          name="name-product"
-          placeholder="Print product"
-        ></input>
-        <h3 className="add-button-text-h">Value:</h3>
-        <input
-          className="add-button-input"
-          type="number"
-          id="name-product"
-          name="name-product"
-          placeholder="Print product"
-        ></input>
-        <h3 className="add-button-text-h">Expiration date:</h3>
-        <input
-          className="add-button-input"
-          type="date"
-          id="name-product"
-          name="name-product"
-          placeholder="Print product"
-        ></input>
+        <div className="add-button-group-div">
+          <h3 className="add-button-text-h">Product:</h3>
+          <input
+            className="add-button-input"
+            type="text"
+            id="name-product"
+            name="name-product"
+            placeholder="Print product"
+          ></input>
+        </div>
+
+        <div className="add-button-group-div">
+          <h3 className="add-button-text-h">Value:</h3>
+          <input
+            className="add-button-input"
+            type="number"
+            id="name-product"
+            name="name-product"
+            placeholder="Print product"
+          ></input>
+        </div>
+
+        <div className="add-button-group-div">
+          <h3 className="add-button-text-h">Expiration date:</h3>
+          <input
+            className="add-button-input"
+            type="date"
+            id="name-product"
+            name="name-product"
+            placeholder="Print product"
+          ></input>
+        </div>
+
         <button className="add-button-but">
           <span>To Add</span>
         </button>
